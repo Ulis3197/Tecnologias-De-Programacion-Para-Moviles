823a82e26797685264c0cfb16ccf0111f03851f9
Ulis3197 <rojasulises30@gmail.com>
2023-08-28 08:56:48 -0600

    Avance

--- input/src/components/Login/LoginHeader.jsx
+++ input/src/components/Login/LoginHeader.jsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Login() {
     return (
         <View>
-            <Text>Login Header</Text>
+            <Text>Login Header Tarea</Text>
         </View>
     );
 }
