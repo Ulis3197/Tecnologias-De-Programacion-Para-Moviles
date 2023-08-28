72506ef054744026a6895ca69455b1a217190cb4
Ulis3197 <rojasulises30@gmail.com>
2023-08-28 08:55:49 -0600

    algo

---
+++ input/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Dimensions, TextInput } from 'react-native';
+import Login from './src/screens/Login';
+
+export default function App() {
+  const {height,width} = Dimensions.get('screen');
+
+  return (
+    <View style={styles.container}>
+      <Login/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
