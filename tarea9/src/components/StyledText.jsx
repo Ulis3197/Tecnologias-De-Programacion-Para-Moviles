91db6a0a7ed4113e714b5286beb00ec2e82e105c
Ulis3197 <rojasulises30@gmail.com>
2023-09-11 21:38:01 -0600

    practica10

--- tarea9/src/components/StyledText.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-export default function StyledText({text, fontSize, color, fontWeight}) {
-  return (
-    <Text style={{fontSize, color, fontWeight}}>{text}</Text>
-  )
-}
